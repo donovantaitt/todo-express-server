88039ac6774b0b2357281e954e91a46cbae8d004
Donovan Taitt <donovan.w.taitt@gmail.com>
2019-05-08 08:06:24 -0400

    setup db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const DB = 'todoapp'
 const PORT = process.env.PORT || 3000
+
+mongoose.connect(`mongodb://localhost:27017/${DB}`, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log(`connected to ${DB} database`));
 
 app.get('/', (req, res) => res.send('running...'))
 
